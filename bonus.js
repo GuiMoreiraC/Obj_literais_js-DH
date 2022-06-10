90557b4d2814be98187588e57248b350d1705f65
GuiMoreiraC <guilherme.moreira.c@protonmail.com>
2022-06-10 20:13:08 -0300

    tentando fazer o bunus

---
+++ bonus.js
@@ -0,0 +1,16 @@
+let array = [
+    { nome: 'Lean', idade: 27 },
+    { nome: 'Eze', idade: 49 },
+];
+
+function propriedadeUnica(array, propriedade) {
+    let novoArray = [];
+    for (let i = 0; i < array.length; i++) {
+        novoArray.push(array[i][propriedade]);
+    }
+    console.log(novoArray);
+    return novoArray;
+}
+
+propriedadeUnica(array, 'idade'); // deve retornar [ { idade: 27 }, { idade: 49 } ]
+propriedadeUnica(array, 'nome'); // deve retornar [ { nome: "Lean"}, { nome: "Eze" } ]
